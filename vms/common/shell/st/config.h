850213e6e3bbbcc4ef57326b431f40bd60e6024e
Alex Jordan <a.l.jordan@protonmail.com>
2018-06-24 16:31:06 +0200

    true color enable

--- vms/common/shell/st/config.h
+++ vms/common/shell/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font:style=Regular:pixelsize=16:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font:style=Regular:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -133,46 +133,6 @@ static unsigned int defaultrcs = 257;
 static unsigned int defaultitalic = 7;
 static unsigned int defaultunderline = 7;
 
-
-
-/* /\* Terminal colors (16 first used in escape sequence) *\/ */
-/* static const char *colorname[] = { */
-/* 	/\* 8 normal colors *\/ */
-/* 	"black", */
-/* 	"red3", */
-/* 	"green3", */
-/* 	"yellow3", */
-/* 	"blue2", */
-/* 	"magenta3", */
-/* 	"cyan3", */
-/* 	"gray90", */
-
-/* 	/\* 8 bright colors *\/ */
-/* 	"gray50", */
-/* 	"red", */
-/* 	"green", */
-/* 	"yellow", */
-/* 	"#5c5cff", */
-/* 	"magenta", */
-/* 	"cyan", */
-/* 	"white", */
-
-/* 	[255] = 0, */
-
-/* 	/\* more colors can be added after 255 to use with DefaultXX *\/ */
-/* 	"#cccccc", */
-/* 	"#555555", */
-/* }; */
-
-
-/* /\* */
-/*  * Default colors (colorname index) */
-/*  * foreground, background, cursor, reverse cursor */
-/*  *\/ */
-/* unsigned int defaultfg = 7; */
-/* unsigned int defaultbg = 0; */
-/* static unsigned int defaultcs = 256; */
-/* static unsigned int defaultrcs = 257; */
 
 /*
  * Default shape of cursor
