37fc8fac5350bef3e1f7cba9557385cb00c2a2d0
Alex Jordan <a.l.jordan@protonmail.com>
2018-11-12 13:01:43 +0100

    other indentation

--- push.sh
+++ push.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 if [[ $EUID -ne 0 ]]; then
-  echo "Must be run as root!"
-  exit 1
+    echo "Must be run as root!"
+    exit 1
 fi
 
 cd /srv/salt
 
 if [ ! -d /srv/salt/.git ]; then
-  git init
-  git remote add origin /home/alj/salt
-  git fetch
-  git checkout -t origin/master
+    git init
+    git remote add origin /home/alj/salt
+    git fetch
+    git checkout -t origin/master
 else
-  git pull
+    git pull
 fi
